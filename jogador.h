0c95886dd498155d157843fc5c352c4b7fe45121
Unknown <amandabarbosa@sjrp.unesp.br>
2017-10-27 17:00:47 -0200

    Código base do avião
    Adição do código base do avião (está mal posicionado)

--- jogador.h
+++ jogador.h
@@ -28,10 +28,46 @@ class Jogador{
 	}
 	
 	void carregar_aviao(){
+
+      	glScalef(20,20,20);
 		
-		glColor3ub( 0, 0, 0);
-		glutWireTeapot(20);
+		glColor3ub(128,128,128); //DEFINE cor (R, G, B e percentual de transparência para o objeto)
+
+	   	/* asas */
+  		glBegin(GL_TRIANGLES); 
+  		glVertex3f(-3.5,0.0,0.0);
+  		glVertex3f(+3.5,0.0,0.0);
+  		glVertex3f(0.0,0.0,3.0);
+      	glScalef(20,20,20);
+  		glEnd();
+  		
+  		/* corpo */
+  		GLUquadricObj *quadric;
+  		quadric = gluNewQuadric();
+  		gluCylinder(quadric, 0.5, 0.5, 4, 12, 3);
+  		
+  		/* nariz */
+  		glPushMatrix();
+  		glTranslatef(0,0,4);
+  		glutSolidCone(0.5, 1.5, 12, 3);
+  		glPopMatrix();
 		
+		/* cauda */
+		glBegin(GL_POLYGON); 
+		glVertex3f(0.0,0.0,0.0);
+		glVertex3f(0.0,2.0,-1.0);
+		glVertex3f(0.0,2.0,0.0);
+		glVertex3f(0.0,0.0,2.0);
+		glEnd();
+	
+	  	/* cabine do piloto */
+	  	glTranslatef(0,0.3,3.5);
+	  	glPushMatrix();
+      	glScalef(0.7,0.7,2.0);
+      	glColor3f(0.3,0.5,1);
+      	glutSolidSphere(0.5, 12, 12);
+      	glPopMatrix();
+
 	}
 	
 	void movimentar(float x){
