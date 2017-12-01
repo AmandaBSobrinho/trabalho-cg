a685d29bd61d7622dc8019e95a8a3ce1da09c6ee
Rodrigo Lima <rodrigo_yc@hotmail.com>
2017-12-01 12:26:01 -0200

    Posicionamento do display para a visao ortogonal e garimpada no código, preparando para a visão em perspectiva

--- jogabilidade.h
+++ jogabilidade.h
@@ -55,107 +55,72 @@ class Jogabilidade{
             if (combustivel <= -40)
                 combustivel += 40;
         }
-        desenhar_medidor(0.1*combustivel);
+
         desenhar_display();
         glutPostRedisplay();
     }
 
     void desenhar_texto (const char *string, float x, float y, float z) {
+
+        glPushMatrix();
+
         glTranslatef (x,y,z);
         glScalef (0.2,0.2,0.2);
         glLineWidth(2);
-        if (vista == 0)
-            glRotatef (90.0f, 1.0f, 0.0f, 0.0f);
         while (*string)
             glutStrokeCharacter (GLUT_STROKE_ROMAN, *string++);
+
+        glPopMatrix();
     }
 
     // Desenha o display cinza onde a pontuação e o combustível devem ficar
     void desenhar_display(){
+
+        desenhar_medidor(0.1*combustivel);
+        
         // Quadro cinza
         glColor3ub(128,128,128);
         glBegin(GL_POLYGON);
-        if (vista) { // se vista = 1, temos a vista superior, e o display fica no plano xy
-            glVertex3f(-400,-400,50);
-            glVertex3f(400,-400,50);
-            glVertex3f(400,-300,50);
-            glVertex3f(-400,-300,50);
-        }
-        else { // se vista = 0, a visão é atrás do avião, e o display fica no plano xz
-            glVertex3f(-400,0,-472);
-            glVertex3f(400,0,-472);
-            glVertex3f(400,0,-368);
-            glVertex3f(-400,0,-368);
-        }
+            glVertex3f(-400,-400,100);
+            glVertex3f(400,-400,100);
+            glVertex3f(400,-300,100);
+            glVertex3f(-400,-300,100);
         glEnd();
 
         // Desenho da pontuação
-        glPushMatrix();
         glColor3ub(255, 255, 0);
         sprintf(escreve_pontos, "%d", pontos);
-        if (vista)
-            desenhar_texto (escreve_pontos, -10, -325, 60);
-        else
-            desenhar_texto (escreve_pontos, -10, -10, -393);
-        glPopMatrix();
+        desenhar_texto (escreve_pontos, -10, -325, 120);
 
         // Desenho do combustível
         glColor3ub(0,0,0);
         glBegin(GL_LINE_LOOP);
-        if (vista){
-            glVertex3f(-100,-380,60);
-            glVertex3f(100,-380,60);
-            glVertex3f(100,-335,60);
-            glVertex3f(-100,-335,60);
-        }
-        else {
-            glVertex3f(-100,-10,-447);
-            glVertex3f(100,-10,-447);
-            glVertex3f(100,-10,-402);
-            glVertex3f(-100,-10,-402);
-        }
+            glVertex3f(-100,-380,120);
+            glVertex3f(100,-380,120);
+            glVertex3f(100,-335,120);
+            glVertex3f(-100,-335,120);
         glEnd();
 
-        glPushMatrix();
-        if (vista)
-            desenhar_texto ("F", 65, -360, 60);
-        else
-            desenhar_texto ("F", 65, -10, -427);
-        glPopMatrix();
+        desenhar_texto ("F", 65, -360, 120);
 
-        glPushMatrix();
-        if (vista)
-            desenhar_texto ("1/2", -25, -360, 60);
-        else
-            desenhar_texto ("1/2", -25, -10, -427);
-        glPopMatrix();
+        desenhar_texto ("1/2", -25, -360, 120);
 
-        glPushMatrix();
-        if (vista)
-            desenhar_texto ("E", -85, -360, 60);
-        else
-            desenhar_texto ("E", -85, -10, -427);
-        glPopMatrix();
+        desenhar_texto ("E", -85, -360, 120);
     }
 
     void desenhar_medidor (float x){
         glPushMatrix();
+
         glTranslatef(x,0,0);
         glColor3ub(255, 255, 0);
+
         glBegin(GL_POLYGON);
-        if (vista) {
-            glVertex3f(80,-380,55);
-            glVertex3f(70,-380,55);
-            glVertex3f(70,-345,55);
-            glVertex3f(80,-345,55);
-        }
-        else {
-            glVertex3f(80,-5,-447);
-            glVertex3f(70,-5,-447);
-            glVertex3f(70,-5,-412);
-            glVertex3f(80,-5,-412);
-        }
+            glVertex3f(80,-380,110);
+            glVertex3f(70,-380,110);
+            glVertex3f(70,-345,110);
+            glVertex3f(80,-345,110);
         glEnd();
+
         glPopMatrix();
     }
     
